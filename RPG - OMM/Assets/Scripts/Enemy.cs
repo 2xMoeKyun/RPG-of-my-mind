15c313700b54c3cdef42892e3822ef543587f1ee
ANullchance <92113036+ANullchance@users.noreply.github.com>
2022-02-10 21:57:49 +0300

    gfdg

--- RPG - OMM/Assets/Scripts/Enemy.cs
+++ RPG - OMM/Assets/Scripts/Enemy.cs
@@ -4,137 +4,71 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    //Other
+    private Transform target;
     private Animator anim;
-    private Rigidbody2D rb;
-    public GameObject player;
-
     //Follow
-    float speed = 1f;
-    float followDistance = 5f;
-    // Atack
-    float atackDistance = 1.5f;
-    float atackSpeed = 3f;
-    // position y
-    public float Ydistance = 0;
-    // recharge
+    public float speed = 1f;
+    public float followDistance = 5f;
+    public float _followDistance = 1f; // Stop Follow Distance 
+    //Atack
+    public bool isAtack = false;
+    public float atkDistance = 1f;
+    public float atkSpeed = 3f;
+    // Atk reload
     float recharge = 0f;
     float startRecharge = 2f;
 
     void Start()
     {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         anim = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody2D>();
-        player = GameObject.Find("Hero");
     }
 
-    bool isAtack = false;
     void Update()
     {
-        float distantToPlayer = Vector2.Distance(transform.position, player.transform.position);
-        if (!isAtack)
+        if((Vector2.Distance(transform.position, target.position) < followDistance) && !isAtack)
         {
-
-            if (distantToPlayer < followDistance)
-            {
-                Follow();
-            }
-            else
-            {
-                StopFollow();
-            }
+            Follow();
         }
-        if (distantToPlayer < atackDistance)
+        if(Vector2.Distance(transform.position, target.position) < atkDistance)
         {
             Atack();
         }
         else
         {
-            
-            StopAtack();
+            isAtack = false;
         }
     }
 
     void Atack()
     {
         isAtack = true;
-
+       
         if (recharge <= 0)
         {
-            GroundCheck();
-            if (player.transform.position.x < transform.position.x)// идет влево
-            {
-                rb.velocity = new Vector2(-atackSpeed, Ydistance);
-                GetComponent<SpriteRenderer>().flipX = true;
-            }
-            if (player.transform.position.x > transform.position.x)
-            {
-                rb.velocity = new Vector2(atackSpeed, Ydistance);
-                GetComponent<SpriteRenderer>().flipX = false;
-            }
             anim.Play("Atack");
             recharge = startRecharge;
         }
         else
         {
-
             recharge -= Time.deltaTime;
         }
     }
-
-    void StopAtack()
-    {
-        isAtack = false;
-        anim.SetBool("Atack", false);
-    }
     void Follow()
     {
-        GroundCheck();
-        if (player.transform.position.x < transform.position.x)// идет влево
+        if (Vector2.Distance(transform.position, target.position) > _followDistance)
         {
-            rb.velocity = new Vector2(-speed, Ydistance);
-            GetComponent<SpriteRenderer>().flipX = true;
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
         }
-        else if (player.transform.position.x > transform.position.x)
+        if (transform.position.x < target.position.x) // vpravo
         {
-            rb.velocity = new Vector2(speed, Ydistance);
             GetComponent<SpriteRenderer>().flipX = false;
         }
-    }
-    void StopFollow()
-    {
-        rb.velocity = new Vector2(0, 0);
-    }
-
-    // Ground check
-    public Transform GrCheck;
-    public LayerMask Ground;
-    bool isGrounded;
-    float CheckRad = 0.5f;
-    GameObject obj;
-    void GroundCheck()
-    {
-        isGrounded = Physics2D.OverlapCircle(GrCheck.position, CheckRad, Ground);
-        obj = GameObject.FindGameObjectWithTag("Ground");
-        if (isGrounded == true)
+        else
         {
-            rb.constraints = RigidbodyConstraints2D.None;
-
-            Ydistance = obj.transform.position.y + 1f;
+            GetComponent<SpriteRenderer>().flipX = true;
         }
-        if (isGrounded == false)
-        {
-            
-            if(obj.transform.position.y < transform.position.y)
-            {
-
-                Ydistance = obj.transform.position.y;
-            }
-            else
-            {
-                rb.constraints = RigidbodyConstraints2D.FreezePositionY;
-            }
-        }
-
     }
-    //
+
 }
