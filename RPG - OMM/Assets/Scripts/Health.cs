df02096c69bb12ad1035a90d9230fd17a774516f
OhShitItsASSpider <eagasymov55@gmail.com>
2022-02-15 14:24:13 +0300

    gdfsg

--- RPG - OMM/Assets/Scripts/Health.cs
+++ RPG - OMM/Assets/Scripts/Health.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     public int health;
     public int maxHealth;
-    
+    public Image healthBarImage;
+    private void Start()
+    {
+        if (GameObject.FindGameObjectWithTag("Player"))
+        {
+            healthBarImage = GetComponent<Image>();
+        }
+    }
     public void TakeHit(int damage)
     {
         health -= damage;
-        if(health <= 0)
+        if (GameObject.FindGameObjectWithTag("Player"))
         {
+            healthBarImage.fillAmount -= 0.1f;
+        }
+        if (health <= 0)
+        {
+            
             //Destroy(gameObject);
         }
     }
+
 }
