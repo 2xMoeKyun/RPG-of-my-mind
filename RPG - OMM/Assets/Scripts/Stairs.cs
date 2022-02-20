389650b794196b99a944ab1dd5e211c729a52b8c
ANullchance <92113036+ANullchance@users.noreply.github.com>
2022-02-21 00:22:04 +0300

    Ladder added
    al mechanics working instead of peremeshenye po x nahodyas na Ladder

---
+++ RPG - OMM/Assets/Scripts/Stairs.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stairs : MonoBehaviour
+{
+    public float speed;
+
+
+    private void Update()
+    {
+        CheckLadder();
+        
+        MoveOnLadder();
+    }
+
+
+    public float CheckRadius;
+    public Transform LadderCheck;
+    public Transform BottomCheck;
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(LadderCheck.position, CheckRadius);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(BottomCheck.position, CheckRadius);
+    }
+
+    public LayerMask Ladder;
+    bool checkedLadder;
+    bool checkedBottom;
+    private void CheckLadder()
+    {
+        checkedLadder = Physics2D.OverlapPoint(LadderCheck.position, Ladder);
+        if (checkedLadder)
+        {
+            Debug.Log("kek");
+        }
+        checkedBottom = Physics2D.OverlapPoint(BottomCheck.position, Ladder);
+        if (checkedBottom)
+        {
+            Debug.Log("lol");
+        }
+    }
+
+    float posY;
+    private void MoveOnLadder()
+    {
+        if (checkedLadder || checkedBottom)
+        {
+            posY = Input.GetAxis("Vertical");
+            Move.playerRb.bodyType = RigidbodyType2D.Kinematic;
+            if (!checkedLadder && checkedBottom)//сверху лестницы
+            {
+                if (posY < 0)
+                {
+                    Move.playerRb.velocity = Vector2.down * speed;
+                }
+            }
+            else if (checkedLadder && checkedBottom)//где-то в лестнице
+            {
+                Move.playerRb.velocity = new Vector2(0, posY * speed);
+            }
+            else if (checkedLadder && !checkedBottom)//снизу
+            {
+                if (posY > 0)
+                {
+                    Move.playerRb.velocity = Vector2.up * speed;
+                }
+            }
+        }
+        else
+        {
+            Move.playerRb.bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+
+}
