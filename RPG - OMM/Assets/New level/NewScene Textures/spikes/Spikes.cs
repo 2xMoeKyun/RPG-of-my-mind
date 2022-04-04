db8b14c40eb03e437b9c285a1d384078194ea8a8
ANullchance <92113036+ANullchance@users.noreply.github.com>
2022-04-04 10:31:02 +0300

    nuasinf

---
+++ RPG - OMM/Assets/New level/NewScene Textures/spikes/Spikes.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    public int Damage;
+    public int PushAwayForce;
+    public GameObject playerCollider2D;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            Health h = collision.gameObject.GetComponent<Health>();
+            h.TakeHit(Damage, playerCollider2D.transform.GetComponent<Collider2D>());
+            Move.playerRb.AddForce(new Vector2(0, collision.transform.position.y + PushAwayForce));
+        }
+    }
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    Move.CanAttack = true;
+    //    Move.CanJump = true;
+    //    Move.CanMove = true;
+    //    Move.CanUse = true;
+    //}
+}
