2f88d58d837401f24b0c79d016bf7e2422aa7140
ANullchance <92113036+ANullchance@users.noreply.github.com>
2022-04-13 00:11:45 +0300

    trying to do boalder with the fx smoke

---
+++ RPG - OMM/Assets/Boulder/Boulder.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boulder : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float startPosY;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        startPosY = transform.GetChild(0).position.y;
+    }
+    private void Update()
+    {
+        transform.GetChild(0).position = new Vector2(transform.position.x - 0.6f, startPosY);
+        transform.GetChild(0).rotation = Quaternion.identity;
+        transform.Rotate(new Vector3(0, 0,-90) * Time.deltaTime);
+        rb.velocity = new Vector2(1, 0);
+        
+    }
+}
