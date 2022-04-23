eb2d1ced73f89f83876dcd846eeffc629ead8435
ANullchance <92113036+ANullchance@users.noreply.github.com>
2022-04-23 15:59:17 +0300

    Rock added

---
+++ RPG - OMM/Assets/NPC Rock/NPC.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    private Animator anim;
+    public List<Transform> pointsToGo;
+    private int currentIndex = 0;
+    public float speed;
+    public static bool setSpecialAnimation;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    private void Update()
+    {
+        if (!stopFollow)
+        {
+        Follow();
+        }
+        if (setSpecialAnimation)
+        {
+            anim.SetBool("Walk", false);
+            SpecialAnimation();
+        }
+    }
+
+
+    private void SpecialAnimation()
+    {
+        anim.SetTrigger("SpecialMove");
+        setSpecialAnimation = false;
+    }
+
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(2f);
+        stopFollow = false;
+    }
+
+    private bool stopFollow;
+    private void SwitchPoint()
+    {
+        anim.SetBool("Walk", false);
+        stopFollow = true;
+        if (currentIndex + 1 < pointsToGo.Count)
+        {
+            currentIndex++;
+            StartCoroutine(Wait());
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private void Follow()
+    {
+        anim.SetBool("Walk", true);
+        if (transform.position.x < pointsToGo[currentIndex].position.x) // vpravo
+        {
+            transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if (Vector2.Distance(transform.position, pointsToGo[currentIndex].position) < 0.3f)
+        {
+            
+            SwitchPoint();
+        }
+
+    }
+}
