f29fd3f05a081a2b3afb72b36a360c316d0dbf18
ANullchance <92113036+ANullchance@users.noreply.github.com>
2022-05-06 20:08:28 +0300

    ора

--- RPG - OMM/Assets/NPC/Trade Mechanics/FillTheSlots.cs
+++ RPG - OMM/Assets/NPC/Trade Mechanics/FillTheSlots.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FillTheSlots : MonoBehaviour
 {
     private SlotManager sm;
     private Bag bag;
+    private RectTransform currentObject;
     private void Start()
     {
         sm = GetComponent<SlotManager>();
@@ -13,17 +15,20 @@ public class FillTheSlots : MonoBehaviour
     }
   
     public void Fill()// width = 51 height = 65
-    {
+    {       
         Debug.Log("Cycelfor");
         for (int i = 0; i < sm.Slots.Length; i++)
         {
-            
-            if (sm.isSlotFull[i] == false)
+            if(i == bag.BagSlots.Length)
             {
-                
-                sm.isSlotFull[i] = true;
-                Instantiate(bag.BagSlots[i], sm.Slots[i].transform);
                 break;
+            }
+            if (sm.isSlotFull[i] == false && bag.BagSlots[i] != null)
+            {
+                currentObject = bag.BagSlots[i].transform.GetChild(1).GetComponent<RectTransform>();
+                currentObject.sizeDelta = new Vector2(currentObject.sizeDelta.x + 51, currentObject.sizeDelta.y + 65);
+                sm.isSlotFull[i] = true;
+                Instantiate(currentObject, sm.Slots[i].transform);
             }
         }
     }
